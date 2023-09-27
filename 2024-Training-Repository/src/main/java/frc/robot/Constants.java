770d87452932a3dc45cb71ff7e7bae8383e6ebe7
Fafnyer <maxcstone1@gmail.com>
2023-09-27 09:39:52 -0500

    create Example subsystem and install vendor deps

---
+++ 2024-Training-Repository/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public final class Constants {
+    
+    public static final int EXAMPLE_MOTOR_CAN_ID = 0;
+
+    public static final Double EXAMPLE_FORWARD_SPEED = 0.50;
+
+    public static final Double EXAMPLE_BACKWARD_SPEED = -0.50;
+}
