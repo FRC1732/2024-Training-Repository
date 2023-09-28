ee5361577cff5acac711bc94998d731435106b03
Fafnyer <maxcstone1@gmail.com>
2023-09-27 19:42:03 -0500

    completed example

--- 2024-Training-Repository/src/main/java/frc/robot/RobotContainer.java
+++ 2024-Training-Repository/src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,45 @@
 
 package frc.robot;
 
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Commands.ExampleCommand;
+import frc.robot.Subsystems.ExampleSubsystem;
 
 public class RobotContainer {
+  public ExampleSubsystem exampleSubsystem;
+
+  private Joystick joystick0;
+
+  private Trigger trigger0;
+
   public RobotContainer() {
-    configureBindings();
+    defineSubsystems();
+    configureButtonBindings();
+
+    trigger0.whileTrue(new ExampleCommand(exampleSubsystem));
   }
 
-  private void configureBindings() {}
+  private void configureButtonBindings() {
+    joystick0 = new Joystick(0);
+
+    trigger0 = new Trigger(() -> joystick0.getRawButton(1));
+  }
+
+  private void defineSubsystems() {
+    exampleSubsystem = new ExampleSubsystem();
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return new InstantCommand(() -> exampleSubsystem.runForward())
+        .andThen(new WaitCommand(2))
+        .andThen(new InstantCommand(() -> exampleSubsystem.stop()));
   }
+
 }
