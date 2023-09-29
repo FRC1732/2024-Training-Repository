ecfe1f0cf2ff6bd2ad84c7e1bba5e8b1475abf97
Alan Bork <aebork@gmail.com>
2023-09-28 20:48:08 -0500

    Session one

---
+++ src/main/java/frc/robot/Subsystems/FancyNewSubsteam.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class FancyNewSubsteam extends SubsystemBase {
+  private CANSparkMax myMotor;
+
+  /** Creates a new FancyNewSubsteam. */
+  public FancyNewSubsteam() {
+    myMotor = new CANSparkMax(Constants.EXAMPLE_MOTOR_CAN_ID, MotorType.kBrushless);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void runForward() {
+    myMotor.set(Constants.EXAMPLE_FORWARD_SPEED);
+  }
+
+  public void runBackward() {
+    myMotor.set(Constants.EXAMPLE_BACKWARD_SPEED);
+  }
+
+  public void stop() {
+    myMotor.set(0);
+  }
+
+}
