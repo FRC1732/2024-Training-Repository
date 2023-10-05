c26b73f249dbde60842b2291443574ec70aeb14b
Fafnyer <maxcstone1@gmail.com>
2023-10-05 18:50:26 -0500

    reset repo

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,11 +3,7 @@ package frc.robot;
 public final class Constants {
     
     public static final int EXAMPLE_MOTOR_CAN_ID = 10;
-    public static final int COOL_NEW_MOTOR__CAN_ID = 11;
 
     public static final Double EXAMPLE_FORWARD_SPEED = 0.50;
     public static final Double EXAMPLE_BACKWARD_SPEED = -0.50;
-
-    public static final Double COOL_NEW_FORWARD_SPEED = 0.50;
-    public static final Double COOL_NEW_BACKWARD_SPEED = -0.50;
 }
