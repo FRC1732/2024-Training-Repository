0beef5b8cae05bf4c2f0ece5f90bf0117edf0eef
Fafnyer <maxcstone1@gmail.com>
2023-10-05 19:35:05 -0500

    Revert "reset repo"
    This reverts commit c26b73f249dbde60842b2291443574ec70aeb14b.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,31 +12,38 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Commands.CoolNewCommand;
 import frc.robot.Commands.ExampleCommand;
+import frc.robot.Subsystems.CoolNewSubsystem;
 import frc.robot.Subsystems.ExampleSubsystem;
 
 public class RobotContainer {
   public ExampleSubsystem exampleSubsystem;
+  public CoolNewSubsystem coolNewSubsystem;
 
   private Joystick joystick0;
 
   private Trigger trigger0;
+  private Trigger trigger1;
 
   public RobotContainer() {
     defineSubsystems();
     configureButtonBindings();
 
     trigger0.whileTrue(new ExampleCommand(exampleSubsystem));
+    trigger1.whileTrue(new CoolNewCommand(coolNewSubsystem));
   }
 
   private void configureButtonBindings() {
     joystick0 = new Joystick(0);
 
     trigger0 = new Trigger(() -> joystick0.getRawButton(2));
+    trigger1 = new Trigger(() -> joystick0.getRawButton(1));
   }
 
   private void defineSubsystems() {
     exampleSubsystem = new ExampleSubsystem();
+    coolNewSubsystem = new CoolNewSubsystem();
   }
 
   public Command getAutonomousCommand() {
