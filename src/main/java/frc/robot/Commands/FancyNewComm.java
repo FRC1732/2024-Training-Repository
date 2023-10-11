8b59f56dcd7e155f0f6770ffc14a44d9adad5ea7
VillarealKaleb <villakal20@muhs.edu>
2023-10-10 20:06:30 -0500

    one last edit 10/10

--- src/main/java/frc/robot/Commands/FancyNewComm.java
+++ src/main/java/frc/robot/Commands/FancyNewComm.java
@@ -16,7 +16,7 @@ public class FancyNewComm extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        fancyNewSub.runForward();
+        fancyNewSub.runBackward();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
