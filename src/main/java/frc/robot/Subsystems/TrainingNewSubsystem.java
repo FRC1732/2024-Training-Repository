8cc9ee37136849843fa2de954083839be8b6cca3
AJ Scheidt <scheidta24@dsha.info>
2023-10-17 20:06:34 -0500

    added shuffleboard

--- src/main/java/frc/robot/Subsystems/TrainingNewSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TrainingNewSubsystem.java
@@ -7,29 +7,55 @@ package frc.robot.Subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class TrainingNewSubsystem extends SubsystemBase {
+  
   private CANSparkMax canSparkMax;
-
+  private double setPoint;
+  
+  private ShuffleboardTab shuffleboardTab;
+  
   /** Creates a new TrainingNewSubsystem. */
   public TrainingNewSubsystem() {
     canSparkMax = new CANSparkMax(10, MotorType.kBrushless);
+    setPoint = 0;
+    setUpShuffleboard();
   }
-
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  public void runforward() {
-    //run motor forward
-    canSparkMax.set( .20);
   
+  public void runforward() {
+    // run motor forward
+    canSparkMax.set(.20);
+    setPoint = .2;
+    
   }
+
   public void stop() {
     canSparkMax.stopMotor();
+    setPoint = 0;
   }
+  
   public void reverse() {
     canSparkMax.set(-.20);
+    setPoint = -.2;
+  }
+
+  public double getSetPoint() {
+    return setPoint;
+  }
+
+  public void setUpShuffleboard() {
+    ShuffleboardTab trainingTab;
+    trainingTab = Shuffleboard.getTab("Training Subsystem");
+    trainingTab.addDouble("Setpoint", () -> getSetPoint());
+    trainingTab.addDouble("Velocity", () -> canSparkMax.getEncoder().getVelocity(  ));
   }
 }
